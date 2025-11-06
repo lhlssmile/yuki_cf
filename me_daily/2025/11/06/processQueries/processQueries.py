0e6396f9f2954944bfe0f51af3d75ce3397bdf3c
lovyouoxx <lhlssmile@gmail.com>
2025-11-06 11:56:14 +0800

    lc3607 DFS lazy-minheap

---
+++ me_daily/2025/11/06/processQueries/processQueries.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+from typing import List
+import sys
+from collections import Counter
+input = lambda: sys.stdin.readline().strip()
+II = lambda: int(input())
+LII = lambda: list(map(int, input().split()))
+SI = lambda: input()
+LSI = lambda: input().split()
+
+
