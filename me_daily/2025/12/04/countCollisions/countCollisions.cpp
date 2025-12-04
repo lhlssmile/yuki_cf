10ba96114b7dc875531fad59b30f928a6acd4755
lovyouoxx <lhlssmile@gmail.com>
2025-12-04 23:11:29 +0800

    lc_daily string

---
+++ me_daily/2025/12/04/countCollisions/countCollisions.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(v) (v).begin(), (v).end()
+#define For(i, n) for (int i = 0; i < (n); ++i)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define Rof(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define EACH(x, v) for (auto& x : (v))
+#define pb push_back
+#define fi first
+#define se second
+#define V vector
+#define SORT(v) sort(ALL(v))
+
+using umpii = unordered_map<int, int>;
+using ll = long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+
+const int inf = 0x3f3f3f3f;
+const ll infl = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+
+int fastio_init = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+int main() {
+    string dirs;cin>>dirs;
+    int n = dirs.size();
+    int l = 0,r=n;
+    while(l<n&&dirs[l]=='L') l++;
+    while(r>l&&dirs[r-1]=='R')r--;
+    int ans = 0;
+    FOR(i,l,r) ans+= dirs[i]!='S';
+    cout<<ans<<"\n";
+    return 0;
+}
+
